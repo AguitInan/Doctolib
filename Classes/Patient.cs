5507c1ed0885280ff66e61cf13416f443bbc5768
Aguit <test@test.com>
2021-02-21 01:43:44 +0100

    Doctolib v1

---
+++ Classes/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doctolib.Classes
+{
+    class Patient
+    {
+        private string codePatient;
+        private string nomPatient;
+        private string adressePatient;
+        private DateTime dateNaissance;
+        private string sexePatient;
+
+        public string CodePatient { get => codePatient; set => codePatient = value; }
+        public string NomPatient { get => nomPatient; set => nomPatient = value; }
+        public string AdressePatient { get => adressePatient; set => adressePatient = value; }
+        public DateTime DateNaissance { get => dateNaissance; set => dateNaissance = value; }
+        public string SexePatient { get => sexePatient; set => sexePatient = value; }
+    }
+}
